df1a81a34f563a5146b221f56d895ff78f534952
AceDJH <460745177@qq.com>
2020-04-03 16:56:54 +0800

    Java控制线程

---
+++ src/thread/MultiThread.java
@@ -0,0 +1,23 @@
+package thread;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+/**
+ * @Author AceDJH
+ * @Date 2020/4/3 16:54
+ * Java 程序天生就是多线程程序，可以通过 JMX 来看一下一个普通的 Java 程序有哪些线程
+ */
+public class MultiThread {
+    public static void main(String[] args) {
+        // 获取 Java 线程管理 MXBean
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        // 不需要获取同步的 monitor 和 synchronizer 信息，仅获取线程和线程堆栈信息
+        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false, false);
+        // 遍历线程信息，仅打印线程 ID 和线程名称信息
+        for (ThreadInfo threadInfo : threadInfos) {
+            System.out.println("[" + threadInfo.getThreadId() + "] " + threadInfo.getThreadName());
+        }
+    }
+}
